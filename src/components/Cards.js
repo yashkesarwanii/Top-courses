0db63d5f10469a202465fa37c5e57e47303db524
yashkesarwaniii <yashkesarwani7393@gmail.com>
2024-10-08 14:41:11 +0530

    Initial commit

---
+++ src/components/Cards.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Card from './Card';
+
+const Cards = ({courses,category}) => {
+    
+    const [likedCourses,setLikedCourses]=useState([]);
+
+    const getCourses=()=>{
+        if(category=="All"){
+            let allCourses=[];
+            Object.values(courses).forEach((array)=>{
+            array.forEach((courseData)=>{
+                allCourses.push(courseData);
+            })
+        })
+        return allCourses;
+        }
+        
+        else{
+            return courses[category];
+        }
+    }
+
+  return (
+    <div className='flex flex-wrap justify-center gap-4 mb-4'>
+        {
+            getCourses().map( (course) =>{
+                return <Card key={course.id} course={course} likedCourses={likedCourses} setLikedCourses={setLikedCourses}/>
+            } )
+        }
+    </div>
+  )
+}
+
+export default Cards
